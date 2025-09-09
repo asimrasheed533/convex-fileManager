ab13f0f30330ac855d90c4a550c9b461e0e49669
Asim Rasheed <asimrasheed442@gmail.com>
2025-09-09 10:42:28 +0500

    Update todo.ts

--- convex/todo.ts
+++ convex/todo.ts
@@ -21,3 +21,25 @@ export const createTodo = mutation({
     });
   },
 });
+
+export const updateTodo = mutation({
+  args: { id: v.id("todo"), task: v.string() },
+  handler: async (ctx, { id, task }) => {
+    await ctx.db.patch(id, { task });
+  },
+});
+
+export const toggleTodo = mutation({
+  args: { id: v.id("todo"), completed: v.boolean() },
+  handler: async (ctx, { id, completed }) => {
+    await ctx.db.patch(id, { completed });
+  },
+});
+
+// export const deleteTodo = mutation{(
+//     args: { id: v.id("todo") },
+//     handler: async(ctx, {id})=>{
+//         await ctx.db.delete(id)
+//     }
+
+// )}
