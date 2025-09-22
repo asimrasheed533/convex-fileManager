9c69b4a5a3b56c66ac98ab12ff2087540dc03230
Asim Rasheed <asimrasheed442@gmail.com>
2025-09-22 11:09:29 +0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,11 @@
 import FileManager from '@/components/file-manager';
-import { cookies } from 'next/headers';
-import { fetchQuery } from 'convex/nextjs';
-import { api } from '@/convex/_generated/api';
-import { redirect } from 'next/navigation';
-import { Id } from '@/convex/_generated/dataModel';
-import AuthProvider from '@/providers/auth';
 import Header from '@/components/header';
 
 export default async function Dashboard() {
-  const cookie = await cookies();
-
-  const token = cookie.get('token')?.value;
-
-  if (!token) {
-    return redirect('/');
-  }
-
-  const user = await fetchQuery(api.user.getCurrentUser, { userId: token as Id<'users'> });
-
-  if (!user) {
-    return redirect('/');
-  }
-
   return (
-    <AuthProvider user={user}>
+    <>
       <Header />
       <FileManager />
-    </AuthProvider>
+    </>
   );
 }
